74a1f79ee6291039607912299126537662d9238e
michaelsnogueira <michaelsnogueira@gmail.com>
2018-11-12 21:32:32 -0200

    Commit my second excercise the basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'my-second-excercise-the-basics';
+  userName = "";
+  buttonDisabled = false;
+
+  onInputEmpty(evento: Event) {
+    console.log(this.buttonDisabled);
+    this.buttonDisabled =
+      (<HTMLInputElement>evento.target).value != "" ? true : false;
+  }
+
+  onClick() {
+    this.buttonDisabled = false;
+    this.userName = "";
+  }
 }
